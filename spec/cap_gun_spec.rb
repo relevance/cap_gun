0a2397d5247fb360e40864ba3401b7f7324ef47b
Rob Sanheim <rsanheim@gmail.com>
2008-06-30 14:54:31 -0400

    no more 'works on my box'

--- spec/cap_gun_spec.rb
+++ spec/cap_gun_spec.rb
@@ -114,15 +114,17 @@ describe "CapGun" do
   
   describe "creating body" do
     it "has a friendly summary line" do
+      CapGun::Mailer.any_instance.stubs(:current_user).returns("jdoe")
       capistrano = { :application => "my app", :rails_env => "staging", :current_release => "/data/foo/releases/20080227120000", :cap_gun_email_envelope => { :from => "booyakka!@example.com", :recipients => ["foo@here.com", "bar@here.com"] } }
       mail = CapGun::Mailer.create_deployment_notification capistrano
-      mail.body.split("\n").first.should == "my app was deployed to staging by rsanheim at February 27th, 2008 8:00 AM EDT."
+      mail.body.split("\n").first.should == "my app was deployed to staging by jdoe at February 27th, 2008 8:00 AM EDT."
     end
 
     it "does not include rails env in summary if not defined" do
+      CapGun::Mailer.any_instance.stubs(:current_user).returns("jdoe")
       capistrano = { :application => "my app", :current_release => "/data/foo/releases/20080227120000", :cap_gun_email_envelope => { :from => "booyakka!@example.com", :recipients => ["foo@here.com", "bar@here.com"] } }
       mail = CapGun::Mailer.create_deployment_notification capistrano
-      mail.body.split("\n").first.should == "my app was deployed by rsanheim at February 27th, 2008 8:00 AM EDT."
+      mail.body.split("\n").first.should == "my app was deployed by jdoe at February 27th, 2008 8:00 AM EDT."
     end
     
   end
