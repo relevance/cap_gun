e221ada327a3f79b583af07175fb1e3dc15c90cc
Sebastian Röbke <sebastian.roebke@xing.com>
2012-11-05 18:41:20 +0100

    Fix presenter example

--- examples/presenter_example.rb
+++ examples/presenter_example.rb
@@ -1,19 +1,19 @@
 require File.join(File.dirname(__FILE__), *%w[example_helper])
 
-describe CapGun::Presenter do 
+describe CapGun::Presenter do
 
   describe "scm details" do
-  
+
     it "does nothing if the scm is not git nor subversion" do
-      presenter = CapGun::Presenter.new(stub("capistrano", :scm => :mercurial))
+      presenter = CapGun::Presenter.new({:scm => :mercurial})
       presenter.scm_details.should == nil
     end
-  
+
     it "collects branch and git log details if git is scm" do
       capistrano = {:scm => :git, :branch => "edge"}
       presenter = CapGun::Presenter.new(capistrano)
       presenter.expects(:scm_log).returns("xxxxx")
-    
+
       details = presenter.scm_details
       details.should include("Branch: edge")
       details.should include("xxxxx")
@@ -58,29 +58,29 @@ describe CapGun::Presenter do
   end
 
   describe "release time" do
-  
+
     before do # make DateTime act as if local timezone is CDT
       @presenter = CapGun::Presenter.new(nil)
       @presenter.stubs(:local_timezone).returns("CDT")
       @presenter.stubs(:local_datetime_zone_offset).returns(Rational(-1,6))
     end
-  
+
     it "returns nil for weird release path" do
       @presenter.humanize_release_time("/data/foo/my_release").should == nil
     end
-  
+
     it "parse datetime from release path" do
       @presenter.humanize_release_time("/data/foo/releases/20080227120000").should == "February 27th, 2008 8:00 AM CDT"
     end
-  
+
     it "converts time from release into localtime" do
       @presenter.humanize_release_time("/data/foo/releases/20080410040000").should == "April 10th, 2008 12:00 AM CDT"
     end
-  
+
   end
 
   describe "from and to emails" do
-    
+
     it "gets recipients from email envelope" do
       capistrano = { :cap_gun_email_envelope => { :recipients => ["foo@here.com", "bar@here.com"] } }
       presenter = CapGun::Presenter.new(capistrano)
@@ -92,25 +92,25 @@ describe CapGun::Presenter do
       presenter = CapGun::Presenter.new(capistrano)
       presenter.from.should == "\"CapGun\" <cap_gun@example.com>"
     end
-  
+
     it "should override sender from email envelope" do
       capistrano = { :cap_gun_email_envelope => { :from => "booyakka!@example.com" } }
       presenter = CapGun::Presenter.new(capistrano)
       presenter.from.should == "booyakka!@example.com"
     end
-    
+
   end
-  
+
   describe "previous_revision" do
-    
+
     it "returns n/a if not set" do
       CapGun::Presenter.new({}).previous_revision.should == "n/a"
     end
-    
+
     it "returns the previous revision if set" do
       CapGun::Presenter.new({:previous_revision => "100"}).previous_revision.should == "100"
     end
-    
+
   end
 
 end
